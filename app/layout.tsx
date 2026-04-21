ffdf27857e17f141a6e28388907f848b7f72723f
Zain <zaindurrani93@gmail.com>
2026-04-21 17:40:58 -0500

    google analytics g4

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,13 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=AW-18109674831"
           strategy="afterInteractive"
         />
-        <Script id="google-ads-tag" strategy="afterInteractive">
+        <Script id="google-ads-and-ga4" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-18109674831');
+            gtag('config', 'G-RYX5S4G874');
           `}
         </Script>
       </head>
